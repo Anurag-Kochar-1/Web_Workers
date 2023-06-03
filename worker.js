3b399d8711ef0ec111104c8fa0167889e3654c10
Anurag <anuragmarketing101@gmail.com>
2023-06-03 08:49:55 +0530

    Learnt: onmessage and postMessage

--- worker.js
+++ worker.js
@@ -1,3 +1,12 @@
 onmessage = (message) => {
-    console.log(message)
+    const sum = calculate()
+    postMessage(sum)
+}
+
+function calculate () {
+    let sum = 0;
+    for(let i = 0; i < 10000000; i++) {
+        sum += i;
+    }
+    return sum  
 }
